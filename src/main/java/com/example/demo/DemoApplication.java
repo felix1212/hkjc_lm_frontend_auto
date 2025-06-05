dfe5603a71ad2bcd60b8c06e0e077c5fdd266891
Felix Wong <felix1212@gmail.com>
2025-06-05 15:58:55 +0800

    v1.0.5 - Add latency settings & minor changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(ApiController.class);
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		logger.info("App Version: 1.0.5");
 	}
 }
